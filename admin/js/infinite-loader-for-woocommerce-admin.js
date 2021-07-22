200be1ba9f4aaa1d1263fe7b6c59cf50a9e8217d
Wbcom dev <pallavi@wbcomdesigns.com>
2021-07-22 13:12:47 +0530

    added loading image functionality

--- admin/js/infinite-loader-for-woocommerce-admin.js
+++ admin/js/infinite-loader-for-woocommerce-admin.js
@@ -135,8 +135,71 @@
 			document.getElementById("infinite-loader_set-default-border-radius-bottom").value = "";
 			document.getElementById("infinite-loader_set-default-border-radius-left").value = "";
 		});
-		
-		
-
+		var infinite_select_icon_for = $('.infinite_display_icon_popup');
+		$(document).on('click', '.infinite_loader_select_fontawesome .infinite_select_icon',function(event) {
+            event.preventDefault();
+			infinite_select_icon_for = $(this);
+			var $html = $('<div class="infinite_loader_select_fontawesome"></div>');
+			$html.append($('.infinite_display_icon_popup'));
+            var $html2 = $('<div class="infinite_loader_icons_display icons_popup_body_area"></div>');
+            $html2.append($html);
+			$('body').children('.icons_popup_body_area').remove();
+            $('body').append($html2);
+			$('.infinite_display_icon_popup').show();
+        });
+		$(document).on('mouseenter', '.infinite_loader_select_fontawesome .infinite_icon_hover', function() {
+            var window_width = $(window).width();
+            window_width = window_width / 2;
+			var $this = $(this).parents('.infinite_fa_fa_icon');
+            if( $this.offset().left < window_width ) {
+				$this.find('.infinite_icon_preview').css({left: '0', right: 'initial'});
+				$this.find('.infinite_icon_preview span').appendTo($this.find('.infinite_icon_preview'));
+            } else {
+				$this.find('.infinite_icon_preview').css({left: 'initial', right: '0'});
+				$this.find('.infinite_icon_preview .fa').appendTo($this.find('.infinite_icon_preview'));
+            }
+        });
+		$(document).on('click', '.infinite_loader_select_fontawesome .infinite_icon_hover',function(event) {
+            event.preventDefault();
+			var value = $(this).parents('.infinite_fa_fa_icon').first().find('.infinite_icon_preview span').text();
+			$(infinite_select_icon_for).parents('.infinite_loader_select_fontawesome').find('.infinite_icon_value').val(value).trigger('change');
+			$('.infinite_display_icon_popup').hide();
+			if ( value ) {
+				$(infinite_select_icon_for).parents('.infinite_loader_select_fontawesome').find('.infinite_selected_icon').html('<i class="fa '+value+'"></i>').show();
+				$(infinite_select_icon_for).parents('.infinite_loader_select_fontawesome').find(".infinite_remove_icons").show();
+			} else {
+				$(infinite_select_icon_for).parents('.infinite_loader_select_fontawesome').find('.infinite_selected_icon').html('').hide();
+				$(infinite_select_icon_for).parents('.infinite_loader_select_fontawesome').find(".infinite_remove_icons").hide();
+			}
+        });
+		$(document).on('click', '.infinite_loader_select_fontawesome .infinite_remove_icon',function(event) {
+            event.preventDefault();
+			$(this).parents('.infinite_loader_select_fontawesome').find('.infinite_selected_icon').html('');
+			$(this).parents('.infinite_loader_select_fontawesome').find('.infinite_icon_value').val('').trigger('change');
+        });
+		$(document).on('keyup', '.infinite_loader_select_fontawesome .infinite_icons_search', function() {
+			var $parent = $(this).parents('.infinite_loader_select_fontawesome').first();
+            var value = $(this).val();
+            value = value.replace(/\s+/g, '');
+            value = value.toLowerCase();
+            if( value.length >=1 ) {
+				$parent.find('.infinite_fa_fa_icon').hide();
+				$parent.find('.infinite_icon_preview span:contains("' + value + '")').parents('.infinite_fa_fa_icon').show();
+            } else {
+				$parent.find('.infinite_fa_fa_icon').show();
+            }
+        });
+		$(document).on('click', '.infinite_loader_select_fontawesome .infinite_display_icon_popup',function(event) {
+            event.preventDefault();
+            $(this).hide();
+        });
+		$(document).on('click', '.infinite_loader_select_fontawesome .infinite_display_icon_popup .infinite_close_popup',function(event) {
+            event.preventDefault();
+			$(this).parents('.infinite_display_icon_popup').hide();
+        });
+		$(document).on('click', '.infinite_loader_select_fontawesome .infinite_icons_popup',function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+        });
 	});
 })( jQuery );
