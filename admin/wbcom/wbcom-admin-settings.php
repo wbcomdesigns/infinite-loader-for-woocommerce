84700cda72ef816806e66030bcad588dff96c32e
Wbcom Dev <pallavi@wbcomdesigns.com>
2022-03-14 13:35:51 +0530

    Fixed phpcs error

--- admin/wbcom/wbcom-admin-settings.php
+++ admin/wbcom/wbcom-admin-settings.php
@@ -528,32 +528,32 @@ if ( ! class_exists( 'Wbcom_Admin_Settings' ) ) {
 			<div id="wb_admin_header" class="wp-clearfix">
 
 				<div id="wb_admin_logo">
-					<img src="<?php echo INFINITE_LOADER_FOR_WOOCOMMERCE_URL . 'admin/wbcom/assets/imgs/logowbcom.png'; ?>">
+					<img src="<?php echo esc_url( INFINITE_LOADER_FOR_WOOCOMMERCE_URL ) . 'admin/wbcom/assets/imgs/logowbcom.png'; ?>">
 					<div class="wb_admin_right"></div>
 				</div>
 
 				<nav id="wb_admin_nav">
 					<ul>
 						<li class="wb_admin_nav_item <?php echo esc_attr( $settings_active ); ?>">
-							<a href="<?php echo get_admin_url() . 'admin.php?page=wbcomplugins'; ?>" id="wb_admin_nav_trigger_settings">
+							<a href="<?php echo esc_url( get_admin_url() ) . 'admin.php?page=wbcomplugins'; ?>" id="wb_admin_nav_trigger_settings">
 								<i class="fas fa-sliders-h"></i>
 								<h4><?php esc_html_e( 'Settings', 'infinite-loader-for-woocommerce' ); ?></h4>
 							</a>
 						</li>
 						<li class="wb_admin_nav_item <?php echo esc_attr( $plugin_active ); ?>">
-							<a href="<?php echo get_admin_url() . 'admin.php?page=wbcom-plugins-page'; ?>" id="wb_admin_nav_trigger_extensions">
+							<a href="<?php echo esc_url( get_admin_url() ) . 'admin.php?page=wbcom-plugins-page'; ?>" id="wb_admin_nav_trigger_extensions">
 								<i class="fas fa-th"></i>
 								<h4><?php esc_html_e( 'Our Plugins', 'infinite-loader-for-woocommerce' ); ?></h4>
 							</a>
 						</li>
 						<li class="wb_admin_nav_item <?php echo esc_attr( $theme_active ); ?>">
-							<a href="<?php echo get_admin_url() . 'admin.php?page=wbcom-themes-page'; ?>" id="wb_admin_nav_trigger_themes">
+							<a href="<?php echo esc_url( get_admin_url() ) . 'admin.php?page=wbcom-themes-page'; ?>" id="wb_admin_nav_trigger_themes">
 								<i class="fas fa-magic"></i>
 								<h4><?php esc_html_e( 'Our Themes', 'infinite-loader-for-woocommerce' ); ?></h4>
 							</a>
 						</li>
 						<li class="wb_admin_nav_item <?php echo esc_attr( $support_active ); ?>">
-							<a href="<?php echo get_admin_url() . 'admin.php?page=wbcom-support-page'; ?>" id="wb_admin_nav_trigger_support">
+							<a href="<?php echo esc_url( get_admin_url() ) . 'admin.php?page=wbcom-support-page'; ?>" id="wb_admin_nav_trigger_support">
 								<i class="fas fa-question-circle"></i>
 								<h4><?php esc_html_e( 'Support', 'infinite-loader-for-woocommerce' ); ?></h4>
 							</a>
