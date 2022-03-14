60baa5bc7f9d03c48137016791aebfa034450481
Wbcom Dev <pallavi@wbcomdesigns.com>
2022-03-14 13:19:24 +0530

    Removed install plugin button from wrapper

--- admin/wbcom/templates/wbcom-plugins-page.php
+++ admin/wbcom/templates/wbcom-plugins-page.php
@@ -40,19 +40,8 @@
 			<div class="reign-demos-inner-wrapper wbcom-plugins-inner-wrapper">
 				<?php
 				foreach ( $free_plugins as $key => $plugin_details ) {
-					if ( 'not_installed' == $plugin_details['status'] ) {
-						$plugin_btn_text = esc_html__( 'Install', 'infinite-loader-for-woocommerce' );
-						$toggle_class    = 'fa fa-toggle-off';
-						$plugin_action   = 'install_plugin';
-					} elseif ( 'installed' == $plugin_details['status'] ) {
-						$plugin_btn_text = esc_html__( 'Activate', 'infinite-loader-for-woocommerce' );
-						$toggle_class    = 'fa fa-toggle-off';
-						$plugin_action   = 'activate_plugin';
-					} else {
-						$plugin_btn_text = esc_html__( 'Deactivate', 'infinite-loader-for-woocommerce' );
-						$toggle_class    = 'fa fa-toggle-on';
-						$plugin_action   = 'deactivate_plugin';
-					}
+						$plugin_btn_text = esc_html__( 'View', 'infinite-loader-for-woocommerce' );
+						$toggle_class    = 'fa fa-eye';
 					?>
 					<div class="wbcom-req-plugin-card">
 						<div class="wbcom_single_left">
@@ -65,12 +54,10 @@
 							<div class="wbcom_single_right-wrap">
 								<p class="plugin-description"><?php echo esc_html( $plugin_details['description'] ); ?></p>
 								<input type="hidden" class="plugin-slug" name="plugin-slug" value="<?php echo esc_attr( $plugin_details['slug'] ); ?>">
-								<input type="hidden" class="plugin-action" name="plugin-action" value="<?php echo esc_attr( $plugin_action ); ?>">
 								<div class="activation_button_wrap">
-									<a href="" class="wbcom-plugin-action-button wb_btn wb_btn_default" >
+									<a href="<?php echo esc_url( $plugin_details['wp_url'] ); ?>" class="wbcom-plugin-action-button wb_btn wb_btn_default" >
 										<i class="<?php echo esc_attr( $toggle_class ); ?>"></i>
 										<?php echo esc_html( $plugin_btn_text ); ?>
-										<i class="fa fa-spinner fa-spin" style="display:none"></i>
 									</a>
 								</div>
 							</div>
