64ff8e660a18a7164502833400817c20e10845a7
Piyush Asthana <pallavi@wbcomdesigns.com>
2022-07-15 11:41:12 +0530

    fixed text domain error

--- admin/partials/infinite-loader-for-woocommerce-welcome-page.php
+++ admin/partials/infinite-loader-for-woocommerce-welcome-page.php
@@ -23,29 +23,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</div><!-- .wbcom-welcome-head -->
 		<div class="wbcom-welcome-content">
 			<div class="wbcom-welcome-support-info">
-				<h3><?php esc_html_e( 'Help &amp; Support Resources', 'bp-checkins' ); ?></h3>
-				<p><?php esc_html_e( 'Here are all the resources you may need to get help from us. Documentation is usually the best place to start. Should you require help anytime, our customer care team is available to assist you at the support center.', 'bp-checkins' ); ?></p>
+				<h3><?php esc_html_e( 'Help &amp; Support Resources', 'infinite-loader-for-woocommerce' ); ?></h3>
+				<p><?php esc_html_e( 'Here are all the resources you may need to get help from us. Documentation is usually the best place to start. Should you require help anytime, our customer care team is available to assist you at the support center.', 'infinite-loader-for-woocommerce' ); ?></p>
 				<div class="wbcom-support-info-wrap">
 					<div class="wbcom-support-info-widgets">
 						<div class="wbcom-support-inner">
-						<h3><span class="dashicons dashicons-book"></span><?php esc_html_e( 'Documentation', 'bp-checkins' ); ?></h3>
-						<p><?php esc_html_e( 'We have prepared an extensive guide on Infinite Loader for Woocommerce to learn all aspects of the plugin. You will find most of your answers here.', 'bp-checkins' ); ?></p>
-						<a href="<?php echo esc_url( 'https://docs.wbcomdesigns.com/doc_category/infinite-loader-for-woocommerce/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Read Documentation', 'bp-checkins' ); ?></a>
+						<h3><span class="dashicons dashicons-book"></span><?php esc_html_e( 'Documentation', 'infinite-loader-for-woocommerce' ); ?></h3>
+						<p><?php esc_html_e( 'We have prepared an extensive guide on Infinite Loader for Woocommerce to learn all aspects of the plugin. You will find most of your answers here.', 'infinite-loader-for-woocommerce' ); ?></p>
+						<a href="<?php echo esc_url( 'https://docs.wbcomdesigns.com/doc_category/infinite-loader-for-woocommerce/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Read Documentation', 'infinite-loader-for-woocommerce' ); ?></a>
 						</div>
 					</div>
 
 					<div class="wbcom-support-info-widgets">
 						<div class="wbcom-support-inner">
-						<h3><span class="dashicons dashicons-sos"></span><?php esc_html_e( 'Support Center', 'bp-checkins' ); ?></h3>
-						<p><?php esc_html_e( 'We strive to offer the best customer care via our support center. Once your theme is activated, you can ask us for help anytime.', 'bp-checkins' ); ?></p>
-						<a href="<?php echo esc_url( 'https://wbcomdesigns.com/support/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Get Support', 'bp-checkins' ); ?></a>
+						<h3><span class="dashicons dashicons-sos"></span><?php esc_html_e( 'Support Center', 'infinite-loader-for-woocommerce' ); ?></h3>
+						<p><?php esc_html_e( 'We strive to offer the best customer care via our support center. Once your theme is activated, you can ask us for help anytime.', 'infinite-loader-for-woocommerce' ); ?></p>
+						<a href="<?php echo esc_url( 'https://wbcomdesigns.com/support/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Get Support', 'infinite-loader-for-woocommerce' ); ?></a>
 					</div>
 					</div>
 					<div class="wbcom-support-info-widgets">
 						<div class="wbcom-support-inner">
-						<h3><span class="dashicons dashicons-admin-comments"></span><?php esc_html_e( 'Got Feedback?', 'bp-checkins' ); ?></h3>
-						<p><?php esc_html_e( 'We want to hear about your experience with the plugin. We would also love to hear any suggestions you may for future updates.', 'bp-checkins' ); ?></p>
-						<a href="<?php echo esc_url( 'https://wbcomdesigns.com/contact/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Send Feedback', 'bp-checkins' ); ?></a>
+						<h3><span class="dashicons dashicons-admin-comments"></span><?php esc_html_e( 'Got Feedback?', 'infinite-loader-for-woocommerce' ); ?></h3>
+						<p><?php esc_html_e( 'We want to hear about your experience with the plugin. We would also love to hear any suggestions you may for future updates.', 'infinite-loader-for-woocommerce' ); ?></p>
+						<a href="<?php echo esc_url( 'https://wbcomdesigns.com/contact/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Send Feedback', 'infinite-loader-for-woocommerce' ); ?></a>
 					</div>
 					</div>
 				</div>
